f6287238c722a927c7a8a512121784d0a761e183
Κώστας Σάκκας <32717524+kostassakkas@users.noreply.github.com>
2017-11-07 21:13:50 +0200

    Add files via upload

---
+++ assets/mario.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="mario" tilewidth="16" tileheight="16" tilecount="14" columns="7">
+ <image source="mario.png" width="112" height="32"/>
+</tileset>
