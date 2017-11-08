3702dfe01baf311dcdf77850b17836f1b07bf925
george2705 <32555232+george2705@users.noreply.github.com>
2017-11-08 11:59:46 +0200

    Delete super_mario_lugi.tsx

--- assets/super_mario_lugi.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="NES - Super Mario Bros - Mario &amp; Luigi" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="912" columns="24">
- <image source="../NES - Super Mario Bros - Mario &amp; Luigi.png" width="447" height="687"/>
-</tileset>
