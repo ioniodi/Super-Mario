87bb737a235749dccdece6b7109e2814777e8594
george2705 <32555232+george2705@users.noreply.github.com>
2017-11-08 11:56:58 +0200

    Delete mario.tsx

--- assets/mario.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="mario" tilewidth="16" tileheight="16" spacing="2" margin="1" tilecount="14" columns="7">
- <image source="mario.png" width="136" height="45"/>
-</tileset>
