c27ee3524fccc16c701721b78f63ad1ed5e44a71
Panos Pipis <32746581+Theblackdemoniv@users.noreply.github.com>
2017-12-10 17:22:25 +0200

    Update MENUS.js

--- MENUS.js
+++ MENUS.js
@@ -1,8 +1,8 @@
 var Menu = {
 	preload: function() {
 
-		game.load.image('Menu', 'assets/menu.png');
-		game.load.image('Stage 1', 'assets/stage 1.png');
+		game.load.image('MENUS', 'assets/menu.png');
+		game.load.image('STAGE1', 'assets/stage 1.png');
 		game.load.audio('tm', 'audio/Rio vs THe XX - Mia NyxtA pOu tAsteRi Mas laMpei oSo KaNeNa.mp3');
 		},
 		
@@ -16,11 +16,11 @@ var Menu = {
 			game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 			game.physics.startSystem(Phaser.Physics.ARCADE);
 			
-			var menuPic = game.add.sprite(8, 8, 'Menu');
+			var menuPic = game.add.sprite(8, 8, 'MENUS');
 
-			var StageText = game.add.text(38, 150, 'Stage1', {fontSize: '12px', fill: '#000'});
-			var click1 = game.add.button(60, 197, 'Stage1', function() {
-			game.state.start('Stage1');
+			var StageText = game.add.text(38, 150, 'STAGE1', {fontSize: '12px', fill: '#000'});
+			var click1 = game.add.button(60, 197, 'STAGE1', function() {
+			game.state.start('STAGE1');
 			tm.stop();
 		});
 		click1.anchor.set(0.5, 0.5);
