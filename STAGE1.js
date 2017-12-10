e21b16ecb528ee68640ba7618f177d755af2c005
Panos Pipis <32746581+Theblackdemoniv@users.noreply.github.com>
2017-12-10 17:16:30 +0200

    Update STAGE1.js

--- STAGE1.js
+++ STAGE1.js
@@ -7,7 +7,7 @@ var Stage1 = {
 			this.load.spritesheet('mario', 'assets/mario.png', 16, 16);
 			this.load.spritesheet('coin', 'assets/coin.png', 16, 16);
            		this.load.image('live','assets/emotion_heart.png');
-            		this.load.spritesheet('new goomba', 'assets/new goomba.png',16, 16);
+            		this.load.spritesheet('goomba1', 'assets/goomba1.png',16, 16);
 			game.load.tilemap('level', 'assets/super_mario_map_new.json', null,
 					Phaser.Tilemap.TILED_JSON);
             //game sound
@@ -46,21 +46,21 @@ var Stage1 = {
 			goombas.setAll('body.bounce.x', 1);
 			goombas.setAll('body.velocity.x', -20);
 			goombas.setAll('body.gravity.y', 500);
-            //new staff(squirtle)
-            new goomba = game.add.group();
-			new goomba.enableBody = true;
-			map.createFromTiles(1, null, 'new goomba', 'enemy', new goomba);
-			new goomba.callAll('animations.add', 'animations', 'walkRight',[ 0, 1 ],2, true);
-			new goomba.callAll('animations.play', 'animations', 'walkRight');
-			new goomba.setAll('body.bounce.x', 1);
-			new goomba.setAll('body.velocity.x', 60);
-			new goomba.setAll('body.gravity.y', 50);
+            //new staff(goomba1)
+           goomba1 = game.add.group();
+			goomba1.enableBody = true;
+			map.createFromTiles(1, null, 'new goomba', 'enemy', goomba1);
+			goomba1.callAll('animations.add', 'animations', 'walkRight',[ 0, 1 ],2, true);
+			goomba1.callAll('animations.play', 'animations', 'walkRight');
+			goomba1.setAll('body.bounce.x', 1);
+			goomba1.setAll('body.velocity.x', 60);
+			goomba1.setAll('body.gravity.y', 50);
             
 			player = game.add.sprite(16, game.world.height - 48, 'mario');
 			game.physics.arcade.enable(player);
 			player.body.gravity.y = 370;
 			player.body.collideWorldBounds = true;
-            player.animations.add('Teleport', [10],10,false);
+           		player.animations.add('Teleport', [10],10,false);
 			player.animations.add('walkRight', [ 1, 2, 3 ], 10, true);
 			player.animations.add('walkLeft', [ 8, 9, 10 ], 10, true);
 			player.goesRight = true;
@@ -68,20 +68,18 @@ var Stage1 = {
 			jumpsound = game.add.sound('jump');
 			coinsound = game.add.sound('coin');
 			stompsound = game.add.sound('stomp');
-            doorsound = game.add.sound('dead');
-            backgroundsound = game.add.sound('background');
-            backgroundsound.loop = true;
-            backgroundsound.play();
-            //score text - fixedtocamera
-            scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '10px', fill: 			'#000' });
-            scoreText.fixedToCamera = true;
-            //mariolives
-            limage=game.add.sprite(140,15,'live');           
-            lcounter=game.add.text(158,20,'X'+lives,{font:'30px',fontSize: '8px',                   fill:'white'});
-            limage.fixedToCamera=true;
+            		doorsound = game.add.sound('door');
+            		backgroundsound = game.add.sound('background');
+            		backgroundsound.loop = true;
+           		backgroundsound.play();
+            		scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '10px', fill: 			'#000' });
+            		scoreText.fixedToCamera = true;
+            		limage=game.add.sprite(140,15,'emotion_heart');           
+            		lcounter=game.add.text(158,20,'X'+lives,{font:'30px',fontSize: '8px',                   fill:'white'});
+            		limage.fixedToCamera=true;
 			lcounter.fixedToCamera=true;
-            if(lives==3){
-			alert("Play!!!");
+            		if(lives==3){
+				alert("Play!!!");
 			}
             
             
@@ -96,8 +94,8 @@ var Stage1 = {
 			game.physics.arcade.collide(player,pipe,pipeOverlap);
 			game.physics.arcade.collide(player, layer);
 			game.physics.arcade.collide(goombas, layer);
-            game.physics.arcade.collide(squirtle, layer);
-            game.physics.arcade.overlap(player, squirtle, squirtleOverlap);
+            		game.physics.arcade.collide(goombas1, layer);
+            		game.physics.arcade.overlap(player, goombas1, goombas1Overlap);
 			game.physics.arcade.overlap(player, goombas, goombaOverlap);
 			game.physics.arcade.overlap(player, coins, coinOverlap);
             
